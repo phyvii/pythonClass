716c997bdefc943e06d736fd3ff8cf13eee0308c
phyvii <gluszczakkrystian@gmail.com>
2023-03-14 09:29:30 +0100

    dodatkowe info

--- classI/main.py
+++ classI/main.py
@@ -1,6 +1,6 @@
 
 a = 1000
-#b = 1000
+b = 1000
 
 b = a
 print(id(a), id(b))
@@ -15,3 +15,28 @@ bool(1)
 float("345")
 float("345.60")
 #przybliżanie do najbliższej liczbt parzystej
+import math
+a,b,c,d,e = 1.5,2.5,3.5,4.5,5.5
+print(a+b+c+d+e)
+print(round(a)+round(b)+round(c)+round(d)+round(e))
+print(int(a)+int(b)+int(c)+int(d)+int(e))
+print(math.ceil(a)+math.ceil(b)+math.ceil(c)+math.ceil(d)+math.ceil(e))
+#math.ceil -celling zaokrągla w góre
+#math.floor - zaokrągla w dół
+
+a = 20
+#blok kodu
+#zwykle oddalony 4 spacjami a nie tabem
+if a > 21:
+    print("a wieksze od 21")
+else:
+    print("czy a mnijeszcze od 21?")
+
+a = 21
+
+if a > 21:
+    print("a wieksze od 21")
+else:
+    print("a rowne 21")
+
+
